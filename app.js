500a2fac67d8902266238b639c1701409a50f861
MaxE23X <matusalaalemseged411@gmail.com>
2023-09-23 10:28:03 -0400

    practice on functions such as alert and more

---
+++ app.js
@@ -0,0 +1,17 @@
+// var yourName = prompt("What is your Name?");
+// console.log(yourName);
+
+// function sayMyName(myName){
+//     alert("Your Name is" + " "+myName)
+// }
+
+// sayMyName("Matusala")
+
+//using prompt and alert together
+function tellAge(rAge){
+    const rName = prompt("what is your Name?")
+    alert("Your name is" + " " + rName + " and your are " + rAge + " years old.")
+}
+
+tellAge(21)
+
